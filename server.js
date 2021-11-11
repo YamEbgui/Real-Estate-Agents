8bdeed03fe8ad0be7ebd01ebef10fd0562241eda
Yam Ebgui <yam.ebgui@gmail.com>
2021-11-11 15:19:17 +0200

    Add get function for cities that return list of all the cities are exist in database

--- server.js
+++ server.js
@@ -7,5 +7,28 @@ const dotenv = require("dotenv").config();
 const Agent = require("./models/agent");
 
 //mongo url: mongodb+srv://yam:${process.env.PASSWORD}@database.foklg.mongodb.net/practiceData?retryWrites=true&w=majority
+mongoose.connect(
+  `mongodb+srv://yam:yamivgi8947@database.foklg.mongodb.net/practiceData?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+  }
+);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+app.use(cors());
+
+//this route return array of cities in the collection agent of database.
+app.get("/cities", function (req, res) {
+  Agent.find({})
+    .distinct("city") // distinct values
+    .then((cities) => {
+      res.json(cities);
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running..."));
